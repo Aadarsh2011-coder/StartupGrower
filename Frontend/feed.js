0558a4b014f80210a4fccecfface153f4f80242a
Aadarsh2011-coder <aadarshprofessional2011@gmail.com>
2025-11-01 18:54:23 +0530

    better

--- Frontend/feed.js
+++ Frontend/feed.js
@@ -10,7 +10,6 @@ import {
 
 // Firebase Config
 const firebaseConfig = {
-  // apiKey: "AIzaSyBJxLH8S259toa65STB7cahCRgnJ8r2Zmg",
   apiKey: "${FIREBASE_API_KEY}",
   authDomain: "startupgrower-bf739.firebaseapp.com",
   projectId: "startupgrower-bf739",
